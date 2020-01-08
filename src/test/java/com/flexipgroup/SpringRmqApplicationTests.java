062a2887c208f0ea080798059a7225fcc6f8d600
Saidi Adegoke <rasheedsaidi@gmail.com>
2020-01-08 18:08:07 +0100

    Implement sample spring rabbitmq

---
+++ src/test/java/com/flexipgroup/SpringRmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexipgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
